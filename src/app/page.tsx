15dd3f8b85e1f91add14ddbfc91a74bd2962c53b
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-08-03 03:11:36 +0700

    fix: responsive md in page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   };
   return (
     <main className="container mx-auto w-full flex justify-center items-center h-screen">
-      <div className="sm-w-3/12 md:w-full flex flex-col">
+      <div className="sm-w-3/12 md:w-6/12 flex flex-col">
         <Lottie {...options} />
         <div className="text-xl sm:text-2xl md:text-4xl text-center font-bold">Website currently under construction</div>
       </div>
